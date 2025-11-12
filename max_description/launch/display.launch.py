78d7c2653468c15082e053b3face0551cfb37ff5
sierra-95 <michaelmachohi@gmail.com>
2025-11-13 01:14:22 +0300

    launch file is complete

---
+++ max_description/launch/display.launch.py
@@ -0,0 +1,53 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, LaunchConfiguration
+import os
+
+def generate_launch_description():
+
+    model_arg = DeclareLaunchArgument(
+        name='model',
+        default_value=os.path.join(
+            get_package_share_directory('max_description'),
+            'urdf',
+            'max.urdf.xacro'
+        ),
+        description='Absolute path to robot urdf file'
+    )
+
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration('model')]), value_type=str)
+
+    joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', os.path.join(
+            get_package_share_directory('max_description'),
+            'rviz',
+            'display.rviz'
+        )],
+    )
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{"robot_description": robot_description}],
+        output='screen'
+    )
+    return LaunchDescription([
+        model_arg,
+        joint_state_publisher_gui,
+        rviz_node,
+        robot_state_publisher_node
+    ])
