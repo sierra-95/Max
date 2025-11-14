2b1f6cee9ec2d4bd2e1b7b0db9daebf804c9b9f4
sierra-95 <michaelmachohi@gmail.com>
2025-11-15 00:59:29 +0300

    Car is turning very well

--- max_controller/launch/controller.launch.py
+++ max_controller/launch/controller.launch.py
@@ -1,9 +1,29 @@
-import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
 
 def generate_launch_description():
+
+    use_python_arg = DeclareLaunchArgument(
+        "use_python",
+        default_value="False",
+    )
+
+    wheel_radius_arg = DeclareLaunchArgument(
+        "wheel_radius",
+        default_value="0.0425",
+    )
+
+    wheel_separation_arg = DeclareLaunchArgument(
+        "wheel_separation",
+        default_value="0.232",
+    )
+
+    use_python = LaunchConfiguration("use_python")
+    wheel_radius = LaunchConfiguration("wheel_radius")
+    wheel_separation = LaunchConfiguration("wheel_separation")
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -24,8 +44,35 @@ def generate_launch_description():
         ]
     )
 
+    simple_controller_py = Node(
+        package="max_controller",
+        executable="simple_controller.py",
+        name="simple_controller",
+        parameters=[
+            {"wheel_radius": wheel_radius},
+            {"wheel_separation": wheel_separation},
+        ],
+        condition=IfCondition(use_python),
+    )
+
+    simple_controller_cpp = Node(
+        package="max_controller",
+        executable="simple_controller",
+        name="simple_controller",
+        parameters=[
+            {"wheel_radius": wheel_radius},
+            {"wheel_separation": wheel_separation},
+        ],
+        condition=UnlessCondition(use_python),
+    )
+
     return LaunchDescription(
         [
+            use_python_arg,
+            wheel_radius_arg,
+            wheel_separation_arg,
+            simple_controller_py,
+            simple_controller_cpp,
             joint_state_broadcaster_spawner,
             simple_controller,
         ]
---
+++ max_controller/max_controller/simple_controller.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32MultiArray
+from geometry_msgs.msg import TwistStamped
+import numpy as np
+
+
+class SimpleController(Node):
+    def __init__(self):
+        super().__init__('simple_controller')
+        
+        self.declare_parameter("wheel_radius", 0.0425)
+        self.declare_parameter("wheel_separation", 0.232)
+
+        self.wheel_radius = self.get_parameter("wheel_radius").get_parameter_value().double_value
+        self.wheel_separation = self.get_parameter("wheel_separation").get_parameter_value().double_value
+
+        self.get_logger().info(f"Wheel Radius: {self.wheel_radius}, Wheel Separation: {self.wheel_separation}")
+
+        self.wheel_cmd_pub_ = self.create_publisher(Float32MultiArray, 'simple_velocity_controller/commands', 10)
+        self.vel_sub_ = self.create_subscription(TwistStamped, 'max_controller/cmd_vel', self.velCallback, 10)
+
+        self.speed_conversion = np.array([[self.wheel_radius/2, self.wheel_radius/2],
+                                          [self.wheel_radius/self.wheel_separation, -self.wheel_radius/self.wheel_separation]])
+    
+        self.get_logger().info(f"The conversion matrix is:\n{self.speed_conversion}")
+    
+    def velCallback(self, msg):
+        robot_speed = np.array([[msg.twist.linear.x],
+                                [msg.twist.angular.z]])
+        
+        wheel_speed = np.matmul(np.linalg.inv(self.speed_conversion), robot_speed)
+        wheel_speed_msg = Float32MultiArray()
+        wheel_speed_msg.data = [wheel_speed[0,0],  # FL  ## 0,0 Left wheel velocity, 0,1 Right wheel velocity
+                                wheel_speed[1,0],  # FR
+                                wheel_speed[0,0],  # RL
+                                wheel_speed[1,0]]  # RR
+        
+        self.wheel_cmd_pub_.publish(wheel_speed_msg)
+
+
+def main():
+    rclpy.init()
+    simple_controller = SimpleController()
+    rclpy.spin(simple_controller)
+    simple_controller.destroy_node()
+    rclpy.shutdown()
+    
+if __name__ == '__main__':
+    main()
+
