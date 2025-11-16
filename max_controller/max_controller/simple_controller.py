4c799846eef52a3cccbf48d7275cf6bec72f8be8
sierra-95 <michaelmachohi@gmail.com>
2025-11-16 17:23:14 +0300

    Broadcasting in Rviz2

--- max_controller/max_controller/simple_controller.py
+++ max_controller/max_controller/simple_controller.py
@@ -2,13 +2,14 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64MultiArray
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import TwistStamped, TransformStamped
 from sensor_msgs.msg import JointState
 import numpy as np
 from rclpy.time import Time
 from rclpy.constants import S_TO_NS
 from nav_msgs.msg import Odometry
 from tf_transformations import quaternion_from_euler
+from tf2_ros import TransformBroadcaster
 import math
 
 class SimpleController(Node):
@@ -47,6 +48,11 @@ class SimpleController(Node):
         self.odom_msg_.pose.pose.orientation.z = 0.0
         self.odom_msg_.pose.pose.orientation.w = 1.0
 
+        self.br_ = TransformBroadcaster(self)
+        self.transform_stamped_ = TransformStamped()
+        self.transform_stamped_.header.frame_id = "odom"
+        self.transform_stamped_.child_frame_id = "base_footprint"
+
         self.get_logger().info(f"The conversion matrix is:\n{self.speed_conversion}")
     
     def velCallback(self, msg):
@@ -55,10 +61,10 @@ class SimpleController(Node):
         
         wheel_speed = np.matmul(np.linalg.inv(self.speed_conversion), robot_speed)
         wheel_speed_msg = Float64MultiArray()
-        wheel_speed_msg.data = [wheel_speed[0,0],  # FL  ## 0,0 Left wheel velocity, 0,1 Right wheel velocity
-                                wheel_speed[1,0],  # FR
-                                wheel_speed[0,0],  # RL
-                                wheel_speed[1,0]]  # RR
+        wheel_speed_msg.data = [wheel_speed[1,0],  # FL  ## 0,0 Left wheel velocity, 0,1 Right wheel velocity
+                                wheel_speed[0,0],  # FR
+                                wheel_speed[1,0],  # RL
+                                wheel_speed[0,0]]  # RR
         
         self.wheel_cmd_pub_.publish(wheel_speed_msg)
 
@@ -74,11 +80,11 @@ class SimpleController(Node):
         fi_left = dp_left / (dt.nanoseconds / S_TO_NS)
         fi_right = dp_right / (dt.nanoseconds / S_TO_NS)
 
-        linear = (self.wheel_radius * fi_left + self.wheel_radius * fi_right) / 2
-        angular = (self.wheel_radius * fi_left - self.wheel_radius * fi_right) / self.wheel_separation
+        linear = (self.wheel_radius * fi_right + self.wheel_radius * fi_left) / 2
+        angular = (self.wheel_radius * fi_right - self.wheel_radius * fi_left) / self.wheel_separation
         
-        d_s = (self.wheel_radius * dp_left + self.wheel_radius * dp_right) / 2
-        d_theta = (self.wheel_radius * dp_left - self.wheel_radius * dp_right) / self.wheel_separation
+        d_s = (self.wheel_radius * dp_right + self.wheel_radius * dp_left) / 2
+        d_theta = (self.wheel_radius * dp_right - self.wheel_radius * dp_left) / self.wheel_separation
         self.theta_ += d_theta
         self.x_ += d_s * math.cos(self.theta_)
         self.y_ += d_s * math.sin(self.theta_)
@@ -93,8 +99,17 @@ class SimpleController(Node):
         self.odom_msg_.pose.pose.position.y = self.y_
         self.odom_msg_.twist.twist.linear.x = linear
         self.odom_msg_.twist.twist.angular.z = angular
+
+        self.transform_stamped_.transform.translation.x = self.x_
+        self.transform_stamped_.transform.translation.y = self.y_
+        self.transform_stamped_.transform.rotation.x = q[0]
+        self.transform_stamped_.transform.rotation.y = q[1]
+        self.transform_stamped_.transform.rotation.z = q[2]
+        self.transform_stamped_.transform.rotation.w = q[3]
+        self.transform_stamped_.header.stamp = self.get_clock().now().to_msg()
         
         self.odom_pub_.publish(self.odom_msg_)
+        self.br_.sendTransform(self.transform_stamped_)
 
 def main():
     rclpy.init()
