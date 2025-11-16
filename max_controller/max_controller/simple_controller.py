c35848f0ddb2eeb2439095df25da7543576fc2bd
sierra-95 <michaelmachohi@gmail.com>
2025-11-16 17:00:50 +0300

    subscribing to max controller odom

--- max_controller/max_controller/simple_controller.py
+++ max_controller/max_controller/simple_controller.py
@@ -7,6 +7,8 @@ from sensor_msgs.msg import JointState
 import numpy as np
 from rclpy.time import Time
 from rclpy.constants import S_TO_NS
+from nav_msgs.msg import Odometry
+from tf_transformations import quaternion_from_euler
 import math
 
 class SimpleController(Node):
@@ -32,10 +34,19 @@ class SimpleController(Node):
         self.wheel_cmd_pub_ = self.create_publisher(Float64MultiArray, 'simple_velocity_controller/commands', 10)
         self.vel_sub_ = self.create_subscription(TwistStamped, 'max_controller/cmd_vel', self.velCallback, 10)
         self.joint_sub_ = self.create_subscription(JointState, 'joint_states', self.jointCallback, 10)
+        self.odom_pub_ = self.create_publisher(Odometry, 'max_controller/odom', 10)
         
         self.speed_conversion = np.array([[self.wheel_radius/2, self.wheel_radius/2],
                                           [self.wheel_radius/self.wheel_separation, -self.wheel_radius/self.wheel_separation]])
     
+        self.odom_msg_ = Odometry()
+        self.odom_msg_.header.frame_id = "odom"
+        self.odom_msg_.child_frame_id = "base_footprint"
+        self.odom_msg_.pose.pose.orientation.x = 0.0
+        self.odom_msg_.pose.pose.orientation.y = 0.0
+        self.odom_msg_.pose.pose.orientation.z = 0.0
+        self.odom_msg_.pose.pose.orientation.w = 1.0
+
         self.get_logger().info(f"The conversion matrix is:\n{self.speed_conversion}")
     
     def velCallback(self, msg):
@@ -72,8 +83,18 @@ class SimpleController(Node):
         self.x_ += d_s * math.cos(self.theta_)
         self.y_ += d_s * math.sin(self.theta_)
         
-        self.get_logger().info(f"Linear Velocity: {linear:.3f} m/s, Angular Velocity: {angular:.3f} rad/s")
-        self.get_logger().info(f"Pose -> x: {self.x_:.3f} m, y: {self.y_:.3f} m, theta: {self.theta_:.3f} rad")
+        q = quaternion_from_euler(0, 0, self.theta_)
+        self.odom_msg_.pose.pose.orientation.x = q[0]
+        self.odom_msg_.pose.pose.orientation.y = q[1]
+        self.odom_msg_.pose.pose.orientation.z = q[2]
+        self.odom_msg_.pose.pose.orientation.w = q[3]
+        self.odom_msg_.header.stamp = self.get_clock().now().to_msg()
+        self.odom_msg_.pose.pose.position.x = self.x_
+        self.odom_msg_.pose.pose.position.y = self.y_
+        self.odom_msg_.twist.twist.linear.x = linear
+        self.odom_msg_.twist.twist.angular.z = angular
+        
+        self.odom_pub_.publish(self.odom_msg_)
 
 def main():
     rclpy.init()
