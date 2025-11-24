90842471bcf5e531633ecee34f6e4d48138f0cc6
sierra-95 <michaelmachohi@gmail.com>
2025-11-24 12:22:49 +0300

    Testing lidar custom implementation

--- max_bringup/launch/max.launch.py
+++ max_bringup/launch/max.launch.py
@@ -2,6 +2,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.conditions import IfCondition, UnlessCondition
@@ -85,18 +86,36 @@ def generate_launch_description():
     slave = GroupAction(
         condition = UnlessCondition(use_master),
         actions = [
-            Node(
-                package='rplidar_ros',
-                executable='rplidar_node',
-                name='rplidar_node',
-                output='screen',
-                parameters=[
+            # Node(
+            #     package='rplidar_ros',
+            #     executable='rplidar_node',
+            #     name='rplidar_node',
+            #     output='screen',
+            #     parameters=[
+            #         os.path.join(
+            #             max_bringup,
+            #             'config',
+            #             'rplidar_a1.yaml'
+            #         )
+            #     ]
+            # ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
                     os.path.join(
-                        max_bringup,
-                        'config',
-                        'rplidar_a1.yaml'
+                        get_package_share_directory("rplidar_ros"),
+                        "launch",
+                        "rplidar_a1_launch.py"
                     )
-                ]
+                ),
+                launch_arguments={
+                    "channel_type": "serial",
+                    "serial_port": "/dev/ttyUSB0",
+                    "serial_baudrate": "115200",
+                    "frame_id": "lidar_link",
+                    "inverted": "false",
+                    "angle_compensate": "true",
+                    "scan_mode": "Sensitivity",
+                }.items()
             ),
             TimerAction(
                 period=4.0,
