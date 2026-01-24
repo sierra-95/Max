01976402d75aeca69e2dbcf89122ea22187a4894
sierra-95 <michaelmachohi@gmail.com>
2026-01-24 23:59:54 +0300

    running on led pi

--- max_utils/max_utils/led_control.py
+++ max_utils/max_utils/led_control.py
@@ -17,6 +17,7 @@ def led_off():
     GPIO.output(RED_PIN, GPIO.LOW)
     GPIO.output(GREEN_PIN, GPIO.LOW)
     GPIO.output(BLUE_PIN, GPIO.LOW)
+    print("LEDs off")
 
 try:
     while True:
@@ -24,42 +25,49 @@ try:
         GPIO.output(RED_PIN, GPIO.HIGH)
         GPIO.output(GREEN_PIN, GPIO.LOW)
         GPIO.output(BLUE_PIN, GPIO.LOW)
+        print("Red ON")
         time.sleep(1)
 
         # Green
         GPIO.output(RED_PIN, GPIO.LOW)
         GPIO.output(GREEN_PIN, GPIO.HIGH)
         GPIO.output(BLUE_PIN, GPIO.LOW)
+        print("Green ON")
         time.sleep(1)
 
         # Blue
         GPIO.output(RED_PIN, GPIO.LOW)
         GPIO.output(GREEN_PIN, GPIO.LOW)
         GPIO.output(BLUE_PIN, GPIO.HIGH)
+        print("Blue ON")
         time.sleep(1)
 
         # Purple (Red + Blue)
         GPIO.output(RED_PIN, GPIO.HIGH)
         GPIO.output(GREEN_PIN, GPIO.LOW)
         GPIO.output(BLUE_PIN, GPIO.HIGH)
+        print("Purple ON")
         time.sleep(1)
 
         # Yellow (Red + Green)
         GPIO.output(RED_PIN, GPIO.HIGH)
         GPIO.output(GREEN_PIN, GPIO.HIGH)
         GPIO.output(BLUE_PIN, GPIO.LOW)
+        print("Yellow ON")
         time.sleep(1)
 
         # Cyan (Green + Blue)
         GPIO.output(RED_PIN, GPIO.LOW)
         GPIO.output(GREEN_PIN, GPIO.HIGH)
         GPIO.output(BLUE_PIN, GPIO.HIGH)
+        print("Cyan ON")
         time.sleep(1)
 
         # White (Red + Green + Blue)
         GPIO.output(RED_PIN, GPIO.HIGH)
         GPIO.output(GREEN_PIN, GPIO.HIGH)
         GPIO.output(BLUE_PIN, GPIO.HIGH)
+        print("White ON")
         time.sleep(1)
 
         # Turn off
@@ -70,3 +78,4 @@ except KeyboardInterrupt:
     # Cleanup GPIO on Ctrl+C
     led_off()
     GPIO.cleanup()
+    print("GPIO cleanup done, program exited")
