3647898352d202b81eddc2ebc193ba0782d6f11e
sierra-95 <michaelmachohi@gmail.com>
2026-01-25 02:01:50 +0300

    running on led pi

--- max_bringup/launch/max.launch.py
+++ max_bringup/launch/max.launch.py
@@ -168,11 +168,6 @@ def generate_launch_description():
                             "use_slam": use_slam,
                             "use_rtab": use_rtab,
                         }.items()
-                    ),
-                    Node(
-                        package="max_utils",
-                        executable="safety_leds.py",
-                        output="screen"
                     )
                 ]
             )
