220c0feff8b02545a8435d26f17c8cb6a23235a5
sierra-95 <michaelmachohi@gmail.com>
2026-01-25 00:10:21 +0300

    running on led pi

--- max_utils/max_utils/led_control.py
+++ max_utils/max_utils/led_control.py
@@ -1,18 +1,19 @@
 import RPi.GPIO as GPIO
 import time
 
-RED_PIN = 17
+TEST_PIN = 19  # The pin you want to toggle
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(RED_PIN, GPIO.OUT)
+GPIO.setup(TEST_PIN, GPIO.OUT)
 
 try:
     while True:
-        GPIO.output(RED_PIN, GPIO.HIGH)
-        print("RED ON")
+        GPIO.output(TEST_PIN, GPIO.HIGH)
+        print("Pin 19 HIGH")
         time.sleep(1)
-        GPIO.output(RED_PIN, GPIO.LOW)
-        print("RED OFF")
+        GPIO.output(TEST_PIN, GPIO.LOW)
+        print("Pin 19 LOW")
         time.sleep(1)
 except KeyboardInterrupt:
     GPIO.cleanup()
+    print("GPIO cleaned up")
