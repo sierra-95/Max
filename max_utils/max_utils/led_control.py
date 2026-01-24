83b0143f783606f1919aa69f3fc53642c626feff
sierra-95 <michaelmachohi@gmail.com>
2026-01-25 00:27:20 +0300

    running on led pi

--- max_utils/max_utils/led_control.py
+++ max_utils/max_utils/led_control.py
@@ -1,18 +1,52 @@
 import RPi.GPIO as GPIO
 import time
 
-LED_PIN = 8
+# Assign GPIO numbers (BCM mode) to RGB pins
+RED_PIN = 4    # Physical pin 7
+GREEN_PIN = 17 # Physical pin 11
+BLUE_PIN = 27  # Physical pin 13
 
+# Setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(RED_PIN, GPIO.OUT)
+GPIO.setup(GREEN_PIN, GPIO.OUT)
+GPIO.setup(BLUE_PIN, GPIO.OUT)
+
+# Function to turn off all LEDs
+def led_off():
+    GPIO.output(RED_PIN, GPIO.LOW)
+    GPIO.output(GREEN_PIN, GPIO.LOW)
+    GPIO.output(BLUE_PIN, GPIO.LOW)
 
 try:
     while True:
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        print("LED is ON")
+        # Red
+        GPIO.output(RED_PIN, GPIO.HIGH)
+        GPIO.output(GREEN_PIN, GPIO.LOW)
+        GPIO.output(BLUE_PIN, GPIO.LOW)
+        print("RED ON")
         time.sleep(1)
-        GPIO.output(LED_PIN, GPIO.LOW)
-        print("LED is OFF")
+
+        # Green
+        GPIO.output(RED_PIN, GPIO.LOW)
+        GPIO.output(GREEN_PIN, GPIO.HIGH)
+        GPIO.output(BLUE_PIN, GPIO.LOW)
+        print("GREEN ON")
         time.sleep(1)
+
+        # Yellow (Red + Green)
+        GPIO.output(RED_PIN, GPIO.HIGH)
+        GPIO.output(GREEN_PIN, GPIO.HIGH)
+        GPIO.output(BLUE_PIN, GPIO.LOW)
+        print("YELLOW ON")
+        time.sleep(1)
+
+        # Turn off
+        led_off()
+        print("ALL OFF")
+        time.sleep(0.5)
+
 except KeyboardInterrupt:
+    led_off()
     GPIO.cleanup()
+    print("Cleanup done, program stopped")
